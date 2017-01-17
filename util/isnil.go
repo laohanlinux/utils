3343ea2cb7e901dafd9096d77e1aaafa6f577429
Rg <daimaldd@gmail.com>
2017-01-18 01:16:45 +0800

    add isnil.go

---
+++ util/isnil.go
@@ -0,0 +1,20 @@
+packge util 
+import (
+	"reflect"
+)
+
+func IsNil(object interface{}) bool {
+	if object == nil {
+		return true
+	}
+
+	// if object type is nil, value will be zero value
+	value := reflect.ValueOf(object)
+	kind := value.Kind()
+	
+	if kind >= reflect.Chan && kind <= reflect.Slice && value.IsNil() {
+		return true
+	}
+
+	return false
+}
