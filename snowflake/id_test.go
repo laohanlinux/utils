fe2681110f4465baefb293e72ac5d12385b70c82
Rg <daimaldd@gmail.com>
2017-03-19 21:33:10 +0800

    update

--- snowflake/id_test.go
+++ snowflake/id_test.go
@@ -3,6 +3,7 @@ package snowflake
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/laohanlinux/assert"
 )
@@ -16,4 +17,21 @@ func TestIDWorker(t *testing.T) {
 	for idx, v := range ids {
 		fmt.Println("idx:", idx, "uid:", v)
 	}
+}
+
+func BenchmarkIDWorker(b *testing.B) {
+
+	idWorker, _ := NewIDWorker(1, 1, 1288834974657)
+
+	var n uint64
+	for i := 0; i < b.N; i++ {
+		if j, err := idWorker.NextID(); err != nil {
+			fmt.Println(err)
+		} else {
+			n++
+			fmt.Println(j, time.Now().Unix(), idWorker.lastTimestamp)
+		}
+	}
+
+	fmt.Println("n", n)
 }
